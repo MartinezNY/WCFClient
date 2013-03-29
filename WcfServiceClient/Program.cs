9e4bee7ed65f935e6e3bdf8e1a51ec8ca339dc0a
Martin Kalyonski <martinkalyonski@yahoo.com>
2013-03-29 19:23:08 +0200

    Test

---
+++ WcfServiceClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WcfServiceClient
+{
+    class Program
+    {
+        /// <summary>
+        /// Test
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
+            //Console.WriteLine(client.GetData(5));
+            string result = client.PutData("", "bike rental", "10", "1", "11", "SITETEST", "2012-04-04 12:00", "2 hours", "1", "1", "1", "1", "1", "1", "123456", "1234415", "martin", "test");
+        }
+    }
+}
